b712522a15206ded1f3e5047e682851e3b5c480b
unknown <fatima24490@cbtis75.edu.mx>
2026-09-21 08:54:34 -0600

    se intalo boostrtap y sweetalert, ademas de comenzar con el archivo de habilidades.ts

--- src/app/componentes/habilidades/habilidades.ts
+++ src/app/componentes/habilidades/habilidades.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PokemonService } from '../../services/pokemon-service';
+import swal from 'sweetalert2';
+
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-habilidades',
   styleUrl: './habilidades.css',
   templateUrl: './habilidades.html',
 })
-export class Habilidades {}
+export class Habilidades {
+  private pokemonService = inject(PokemonService);
+  nombre: string = '';
+  habilidad: any=null;
+
+  buscarHabilidad() {
+    if (!this.nombre.trim()) {
+      swal.fire({
+        icon: "warning",
+        title: "Campo vacío",
+        text: "Escribe el nombre de una habilidad",
+      });
+      return;
+    }
+
+    this.pokemonService.getAbility(this.nombre)
+      .subscribe({
+        next: (data: any) =>{
+          this.habilidad = data;
+        },
+
+        error: () => {
+          this.habilidad = null;
+          swal.fire({
+            icon: "error",
+            title: "Habilidad no encontrada",
+            text: "Intenta nuevamnete"
+          });
+        }
+
+      });
+
+  }
+
+}
--- src/app/services/pokemon-service.ts
+++ src/app/services/pokemon-service.ts
@@ -1,4 +1,22 @@
-import { Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 
-@Service()
-export class PokemonService {}
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  private http = inject(HttpClient);
+  private URL_BASE = 'https://pokeapi.co/api/v2/pokemon/';
+
+  getAbility(nombre: string) {
+    return this.http.get(
+        this.URL_BASE + nombre.toLocaleLowerCase() + '/ability'
+    );
+}
+
+getSpecies(nombre: string) {
+    return this.http.get(
+        this.URL_BASE + nombre.toLocaleLowerCase() + '/species'
+    );
+  }
+}
