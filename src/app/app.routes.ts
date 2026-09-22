4cdc8f94661c8a3f3cef24758c6cf87b1026faa0
unknown <fatima24490@cbtis75.edu.mx>
2026-09-22 08:52:09 -0600

    Agregue las rutas de los archivos de especies y habilidades

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Especies } from './componentes/especies/especies';
+import { Habilidades } from './componentes/habilidades/habilidades';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'habilidades',
+        component: Habilidades
+    },
+
+    {
+        path: 'especies',
+        component: Especies
+    }
+];
