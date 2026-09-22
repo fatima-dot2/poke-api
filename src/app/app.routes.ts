636ea44c0fbf640f9a0a9a38057e42ce5ce4e8b9
unknown <fatima24490@cbtis75.edu.mx>
2026-09-22 09:15:02 -0600

    Se creo los archivos de inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { Especies } from './componentes/especies/especies';
 import { Habilidades } from './componentes/habilidades/habilidades';
+import { Inicio } from './componentes/inicio/inicio';
 
 export const routes: Routes = [
+
+     {
+    path: '',
+    component: Inicio
+     },
+
     {
         path: 'habilidades',
         component: Habilidades
---
+++ src/app/componentes/inicio/inicio.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [RouterLink],
+  templateUrl: './inicio.html'
+})
+export class Inicio {
+
+}
